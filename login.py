fdf57628e8b404fddd31bf51c8f89a7a843ec182
Poornima Geddada <69567812+Poornima-Geddada@users.noreply.github.com>
2020-09-25 16:24:56 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,11 @@
+pid=gedddadapoornima@gmail.com
+ppw=poornima@A
+for i in range(4):
+    if pid=input('enter your email')and ppw=input('enter password'):
+        print('Login Succesful')
+        break
+    else:
+        print('Wrong username or password')
+
+else:
+    print('Entered wrong details too many times, Try later')
